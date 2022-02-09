006763ef523bc0c0d83382952bab71b6f8c38e2b
patlewig <patlewig@users.noreply.github.com>
2022-02-09 15:58:23 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An evaluation of the utility of using AIM features sets relative to other chemical fingerprint sets such as ToxPrints in a read-across context.',
+    author='patlewig',
+    license='MIT',
+)
